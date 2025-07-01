959d1e74ddbf6b23a828467ceff6206ac3133336
virus0231 <usman@youronlineconversation.com>
2025-07-01 18:22:37 +0500

    use state error update

--- src/app/reports/page.js
+++ src/app/reports/page.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import Home from '../home/page'
-import React, { useState } from 'react' 
+'use client'
+import React, { useState } from 'react'
+import Home from '../home/page' 
 
 function page() {
+
+  const [name, setName] = useState("");
+
   return (
     <Home>
       <div className='theme_box donor_box glass_layer h-fit'>
 
         <div className='items_holder theme_inner_box grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
           <div>
+            <p>From:</p>
            <input
-              type="text"
-              value={"Name"}
-              placeholder="Enter your text here"
+              id={"from_date"}
+              type="date"
+              value={name}
+              onChange={(e)=>setName(e.target.value)}
               className="border rounded p-2"
             />
           </div>
