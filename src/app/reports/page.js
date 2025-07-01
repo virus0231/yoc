fee13e3f4fd514a3a82700301ef72b6eff5552d0
virus0231 <usman@youronlineconversation.com>
2025-07-01 18:40:59 +0500

    glass update

--- src/app/reports/page.js
+++ src/app/reports/page.js
@@ -10,16 +10,36 @@ function page() {
     <Home>
       <div className='theme_box donor_box glass_layer h-fit'>
 
-        <div className='items_holder theme_inner_box grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+        <div className='items_holder theme_inner_box grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
           <div>
             <p>From:</p>
-           <input
-              id={"from_date"}
-              type="date"
-              value={name}
-              onChange={(e)=>setName(e.target.value)}
-              className="border rounded p-2"
-            />
+            <input
+                id={"from_date"}
+                type="date"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                className="border rounded p-2"
+              />
+          </div>
+          <div>
+            <p>From:</p>
+            <input
+                id={"from_date"}
+                type="date"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                className="border rounded p-2"
+              />
+          </div>
+          <div>
+            <p>From:</p>
+            <input
+                id={"from_date"}
+                type="date"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                className="border rounded p-2"
+              />
           </div>
         </div>
 
