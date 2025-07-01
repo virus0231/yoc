138445855b58989db2275d0b7386a91802acea61
hunainyoc <hunain@youronlineconversation.com>
2025-07-01 17:53:26 +0500

    renamed globals.css to layout.css and initialized shadcn

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import Home from '../home/page'
+import React from "react";
+import Home from "../home/page";
+import { ChartLineMultiple } from "./income-chart";
 
 function Dashboard() {
   return (
     <Home>
-        <div>dashboard</div>
+      <div>dashboard</div>
+      <div className="glass_layer theme_box">
+        <ChartLineMultiple />
+      </div>
     </Home>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
