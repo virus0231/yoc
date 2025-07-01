d4c0348ff3acb01023ab576081a8f3c96608e502
virus0231 <usman@youronlineconversation.com>
2025-07-01 17:54:52 +0500

    changes from usman

--- src/app/reports/page.js
+++ src/app/reports/page.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Home from '../home/page'
+import React, { useState } from 'react' 
 
 function page() {
   return (
     <Home>
-      <div>page</div>
+      <div className='theme_box donor_box glass_layer h-fit'>
+
+        <div className='items_holder theme_inner_box grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+          <div>
+           <input
+              type="text"
+              value={"Name"}
+              placeholder="Enter your text here"
+              className="border rounded p-2"
+            />
+          </div>
+        </div>
+
+      </div>
     </Home>
   )
 }
