b7658d2f2e969302cabf6b431b0c0df9090aced1
virus0231 <usman@youronlineconversation.com>
2025-07-01 19:35:31 +0500

    minor changes with dashboard pages

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,14 +1,16 @@
 // app/dashboard/page.js
 "use client";
-
+import Home from "../home/page";
 import StackedRevenueBarChart from "./StackedRevenueBarChart";
 
 export default function DashboardPage() {
   return (
-    <main className="min-h-screen px-4 py-6 bg-black text-white">
-      <div className="max-w-7xl mx-auto space-y-6">
-        <StackedRevenueBarChart />
-      </div>
-    </main>
+    <Home>
+      <main className="min-h-screen px-4 py-6 bg-black text-white">
+        <div className="max-w-7xl mx-auto space-y-6">
+          <StackedRevenueBarChart />
+        </div>
+      </main>
+    </Home>
   );
 }
